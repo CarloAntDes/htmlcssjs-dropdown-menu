16ef501eac17e941cb30457d6b13f7ec2d71d2d6
CarloAntDes <carloantoniodesantis@gmail.com>
2019-03-05 15:52:52 +0100

    End parte grafica

---
+++ main.js
@@ -0,0 +1,26 @@
+/*
+Creare un menù a comparsa e scomparsa che abbia al suo interno
+anche dei sotto menù
+*/
+
+function prova() {
+  $("h1").click(function() {
+    alert()
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+function all_function() {
+  prova()
+}
+
+$(document).ready(prova);
